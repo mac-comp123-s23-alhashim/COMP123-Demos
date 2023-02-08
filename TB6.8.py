58942302620a51d311910fe160447168c32f032b
Amin G. Alhashim, PhD, 6σ <amin.g.alhashim@gmail.com>
2023-02-07 22:52:33 -0600

    Example from Textbook 6.8

---
+++ TB6.8.py
@@ -0,0 +1,18 @@
+import turtle
+
+def drawSquare(t: object, sz: object) -> object:
+    """
+    Make turtle t draw a square of width sz
+    """
+    for i in range(4):
+        t.forward(sz)
+        t.left(90)
+
+
+wn = turtle.Screen()
+wn.bgcolor("lightgreen")
+
+alex = turtle.Turtle()
+drawSquare(alex, 50)
+
+wn.exitonclick()
