93904eeaf8ce6587a0db15ba02e0baafb36a4753
Amin G. Alhashim, PhD, 6σ <amin.g.alhashim@gmail.com>
2023-02-16 16:40:35 -0600

    Prep for F 2/17 class

---
+++ TFD_prep.py
@@ -0,0 +1,23 @@
+def sum(num1, num2):
+	"""
+	Sum two number
+
+	Post-condition: return the sum of num1 and num2
+	"""
+	return num1 + num2
+
+
+def test_sum_positives():
+	assert sum(1, 2) == 3
+
+
+def test_sum_negatives():
+	assert sum(-1, -2) == -3
+
+
+def test_sum_zeros():
+	assert sum(0, 0) == 0
+
+
+def test_sum_mix():
+	assert sum(2, -2) == 0
