0dcf01ff2dc0504241ccecb3cc5e65762a693a7f
Amin G. Alhashim, PhD, 6σ <amin.g.alhashim@gmail.com>
2023-02-17 16:12:56 -0600

    Update unit tests

--- TFD/ourFunction.py
+++ TFD/ourFunction.py
@@ -1,4 +1,6 @@
 import math
+import pytest
+
 
 def our_sqrt(x):
     """
@@ -7,20 +9,26 @@ def our_sqrt(x):
     pre-condition: x must be positive
     post-condition: return square root of x
     """
-
     assert type(x) == int
     assert x >= 0
     return math.sqrt(x)
 
-def test_our_sqrt_postive():
+
+def test_our_sqrt_positive():
     assert our_sqrt(16) == 4
+
 
 def test_our_sqrt_zero():
     assert our_sqrt(0) == 0
 
+
+# catching assertions raised by the method
+# you don't need to do this
 def test_our_sqrt_negative():
-    our_sqrt(-16)
+    with pytest.raises(Exception) as e_info:
+        our_sqrt(-16)
+
 
 # if you have pytest package install, you don't need the lines below
-# test_our_sqrt_postive()
-# test_our_sqrt_zero()
+test_our_sqrt_positive()
+test_our_sqrt_zero()
