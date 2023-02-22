37f0adf899637dc3b3f3c350d025f4ea980d6086
Amin G. Alhashim, PhD, 6σ <amin.g.alhashim@gmail.com>
2023-02-22 12:13:10 -0600

    F 2/22 class on zoom

--- strings/guess_output.py
+++ strings/guess_output.py
@@ -1,22 +1,22 @@
-# def print_seperator(msg: str):
-#     sep = "="
-#     msg = msg.upper()
-#     msg = msg.center(len(msg)+2)
-#     msg = sep*5 + msg
-#     msg = msg.ljust(80, sep)
-#     print()
-#     print(msg)
+def print_seperator(msg: str):
+    sep = "="
+    msg = msg.upper()
+    msg = msg.center(len(msg)+2)
+    msg = sep*5 + msg
+    msg = msg.ljust(80, sep)
+    print()
+    print(msg)
 
 
-# print_seperator("original")
+print_seperator("original")
 welcome_msg = "welcome to class python software developers"
 print(welcome_msg)
 
-# print_seperator("upper case")
-welcome_msg.upper()
-print(welcome_msg)
+print_seperator("upper case")
+welcome_msg_cap = welcome_msg.upper()
+print(welcome_msg_cap)
 
-# print_seperator("title")
-welcome_msg.title()
-print(welcome_msg)
+print_seperator("title")
+welcome_msg_title = welcome_msg.title()
+print(welcome_msg_title)
 
