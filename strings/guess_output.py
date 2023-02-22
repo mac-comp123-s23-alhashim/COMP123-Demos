e04249cf7e8663f0a6784a2d62d3183e51fbcfc1
Amin G. Alhashim, PhD, 6σ <amin.g.alhashim@gmail.com>
2023-02-22 07:59:37 -0600

    Prep for W 2/22 class

--- strings/guess_output.py
+++ strings/guess_output.py
@@ -1,9 +1,22 @@
+def print_seperator(msg: str):
+    sep = "="
+    msg = msg.upper()
+    msg = msg.center(len(msg)+2)
+    msg = sep*5 + msg
+    msg = msg.ljust(80, sep)
+    print()
+    print(msg)
+
+
+print_seperator("original")
 welcome_msg = "welcome to class python software developers"
 print(welcome_msg)
 
+print_seperator("upper case")
 welcome_msg.upper()
 print(welcome_msg)
 
+print_seperator("title")
 welcome_msg.title()
 print(welcome_msg)
 
