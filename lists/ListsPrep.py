4b6b3d3dbc76a3868e27485d3c6355e28b785390
Amin G. Alhashim, PhD, 6σ <amin.g.alhashim@gmail.com>
2023-02-26 16:21:11 -0600

    Prep for F 2/27 class

---
+++ lists/ListsPrep.py
@@ -0,0 +1,66 @@
+# list of ints
+list1 = [1, 10, 16]
+print(list1)
+
+
+# list of strings
+list2 = ['comp123', 'is', 'a', 'fun', 'course']
+print(list2)
+
+
+# list of mix data types
+list3 = ['Temp', 2023, 3, 27, 'is', 37.1, 'F']
+print(list3)
+
+
+# list of lists
+list4 = [list1, list2, list3]
+print(list4)
+
+
+# empty list
+list5 = []
+print(list5)
+
+
+# add lists together
+list6 = list1 + list2 + list3
+print(list6)
+
+
+# repeat lists
+list7 = list1 * 3
+print(list7)
+
+
+# update an element in the list
+list7[2] = list7[2] * 100
+print(list7)
+
+
+# update a portion of a list
+list7[0:2] = [1.1, 2.2]
+print(list7)
+
+
+# remove a portion from a list
+list7[0:4] = []
+print(list7)
+
+
+# remove a portion from a list
+del list7[2:]
+print(list7)
+
+
+# insert element to a list
+list7[1:1] = [5.5, 6.6, 7.7]
+print(list7)
+
+
+# concatenation vs append
+list7 + [3]
+print(list7)
+
+list7.append(7)
+print(list7)
