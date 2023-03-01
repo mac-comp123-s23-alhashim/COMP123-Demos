10264d51f207c57bd10c44ed8aefec42b2c5b9a4
Amin G. Alhashim, PhD, 6σ <amin.g.alhashim@gmail.com>
2023-03-01 11:24:03 -0600

    3/1 class

--- dict/DictPrep.py
+++ dict/DictPrep.py
@@ -1,43 +1,43 @@
 # dictionary of str-float pairs ================================================
 dict1 = {'cake': 3.99, 'muffin': 4.99, 'pizza': 2.99}
-# print(dict1)
+print(dict1)
 
 
 # getting the value associated with a key ======================================
-# print(dict1['cake'])
-# print(dict1['muffin'])
+print(dict1['cake'])
+print(dict1['muffin'])
 
 
 # updating the value associated with a key =====================================
-# dict1['cake'] = dict1['cake'] + 1
-# print(dict1)
+dict1['cake'] = dict1['cake'] + 1
+print(dict1)
 
 
 # deleting a key-value pair ====================================================
-# del dict1['cake']
-# print(dict1)
+del dict1['cake']
+print(dict1)
 
 
 # getting number of key-value pairs (dictionary size) ==========================
-# print(len(dict1))
+print(len(dict1))
 
 
 # dictionary methods ===========================================================
-# print(dict1.keys())         # keys in dictionary
-# print(dict1.values())       # values in dictionary
-# print(dict1.items())        # view of key-value pair tuples
-# print(dict1.get('muffin'))  # get value associated with the key, otherwise
+print(dict1.keys())         # keys in dictionary
+print(dict1.values())       # values in dictionary
+print(dict1.items())        # view of key-value pair tuples
+print(dict1.get('muffin'))  # get value associated with the key, otherwise
 # None
 
 
 # get vs [] operator when a key does not exist =================================
-# print(dict1.get('banana'))
+print(dict1.get('banana'))
 # print(dict1['banana'])
 
 
 # iterate over a dictionary ====================================================
-# for key in list(dict1.keys()):      # can be simplified to for key in dict1:
-#     print(key, '\t', dict1[key])
+for key in dict1:      # can be simplified to for key in dict1:
+    print(key, '\t', dict1[key])
 
 
 # dictionary of str-str pairs ==================================================
