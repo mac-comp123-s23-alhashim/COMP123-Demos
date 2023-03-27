bd688be20852b7879227133d7ac172a89a113b6f
Amin G. Alhashim, PhD, 6σ <amin.g.alhashim@gmail.com>
2023-03-27 09:33:31 -0500

    resources added

---
+++ data/mediasources-4ed/_README.md
@@ -0,0 +1,1 @@
+Source: https://web.eecs.umich.edu/~mjguz/mediacomp/mediaComp-teach/1#Python
--- images_prep/prep.py
+++ images_prep/prep.py
@@ -2,11 +2,15 @@ from imageTools import *
 import PIL
 
 # font = pickAFont()
-font = ImageFont.truetype("Demo_ConeriaScript.ttf", 300)
+# font = ImageFont.truetype("Demo_ConeriaScript.ttf", 300)
+#
+# mac_logo = Picture("mac-logo.png")
+# mac_logo.drawText(mac_logo.getWidth()/2, mac_logo.getHeight()/2, "Mac",
+#                   "yellow", font)
+# mac_logo.show()
 
-mac_logo = Picture("mac-logo.png")
-mac_logo.drawText(mac_logo.getWidth()/2, mac_logo.getHeight()/2, "Mac",
-                  "yellow", font)
-mac_logo.show()
+turtle_pic = Picture("../data/mediasources-4ed/turtle.jpg")
+turtle_pic.show()
+print(turtle_pic.getColor(0,0))
 
 input()
