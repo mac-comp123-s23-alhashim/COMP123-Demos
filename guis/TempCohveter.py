9a4ca704f6a9b9d13c6fc0f506e1235bedb3c9be
Amin G. Alhashim, PhD, 6σ <amin.g.alhashim@gmail.com>
2023-04-11 14:39:40 -0500

    add comments

--- guis/TempCohveter.py
+++ guis/TempCohveter.py
@@ -1,21 +1,26 @@
 import tkinter as tk
 
+
 def convert():
     temp_c = float(input_c.get())
-    temp_f = temp_c * 9/5 + 32
+    temp_f = temp_c * 9 / 5 + 32
     label['text'] = temp_f
+
 
 # window
 window = tk.Tk()
 
+# text filed to read temp in ˚C
 input_c = tk.Entry(window)
 input_c.grid(row=0, column=0)
 
+# button
 button_covert = tk.Button(window, text="Convert", command=convert)
 button_covert.grid(row=0, column=1)
 
+# label to show temp in ˚F
 label = tk.Label(window)
 label.grid(row=1, column=0)
 
-# run
+# run the app
 window.mainloop()
